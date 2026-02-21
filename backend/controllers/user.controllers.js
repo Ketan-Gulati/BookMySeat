aeb486303a0c0945c97fa4dbf946399eddd45988
Ketan Gulati <ketangulati66@gmail.com>
2026-02-22 02:00:23 +0530

    created login controller

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -73,4 +73,42 @@ const registerUser = asyncHandler(async(req, res)=>{
     )
 })
 
-export {registerUser}
+const loginUser = asyncHandler(async(req, res)=>{
+    const {userName, email, password} = req.body;
+
+    if(!(email || userName)){
+        throw new ApiError(400, "Username or email is required");
+    }
+
+    const user = await User.findOne({
+        $or: [{email}, {userName}]
+    })
+
+    if(!user) throw new ApiError(401, "User not found");
+
+    const isPasswordValid = await user.isCorrectPassword(password);
+
+    if(!isPasswordValid) throw new ApiError(401, "Invalid user credentials");
+
+    const {accessToken, refreshToken} = generateTokens(user._id);
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
+
+    const options = {
+        httpOnly: true,
+        /* secure: true,
+        sameSite: "None", */
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+    }
+
+    return res.
+    status(200)
+    .cookie("accessToken",accessToken,options) 
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(200, "User logged in successfully", loggedInUser)
+    )
+})
+
+export {registerUser, loginUser};
